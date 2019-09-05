0331f2e0209b9e6263aac992a3a26ccc24b9e546
Nimrod Garcia <nimm.garcia@gmail.com>
2019-09-05 15:10:40 -0600

    Add comment for keyword definitions

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,11 @@
+// - public: access modifier
+// - class: defines a class, {...} defines class body
+public class Hello {
+    // - methods: collection of statements that perform an operation
+    // static -
+    // void - indicates the method will not return anything
+    public static void main(String[] args) {
+        // Statement - complete command to be executed & can include one/more expression
+        System.out.println("Hello Nim");
+    }
+}
